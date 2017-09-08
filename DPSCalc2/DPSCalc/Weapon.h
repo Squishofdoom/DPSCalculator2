b5b0704d807433444d2f4b7ccf02932b30b5edd7
Squishofdoom <spamisbadjerk@live.com>
2017-09-08 11:07:41 -0600

    Added Weapon class, class variables, empty declaration file, empty source file

---
+++ DPSCalc2/DPSCalc/Weapon.h
@@ -0,0 +1,35 @@
+#include <string>
+using namespace std;
+
+class Weapon {
+
+private:
+
+string weaponType;
+
+int fireRate;
+int velocity;
+
+float headshotMultiplier;
+float moveSpeedMultiplier;
+
+int maxDamage;
+int maxDamageRange;
+
+int minDamage;
+int minDamageRange;
+
+float shortReload;
+float longReload;
+
+int magazineSize;
+int totalAmmo;
+
+public:
+
+	//Mutators
+
+
+	//Accessors
+
+};
